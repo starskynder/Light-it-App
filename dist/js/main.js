fef0c8a662864c20873eb4a83c9dec1539ea5093
starskynder <24276076+starskynder@users.noreply.github.com>
2018-08-01 16:59:47 +0200

    Add logic to feedback slideshow section

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,3 +27,41 @@ function showHideMenu() {
     v.classList.toggle("js-menu-line--cross");
   });
 }
+
+//Select feedback boxes
+const feedbackBoxes = document.querySelectorAll(".feedback__box");
+
+//Select feedback slideshow arrows
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
+
+//Set index of first slide to zero and call slideshow function
+let slideIndex = 0;
+slideImage(slideIndex);
+
+//Add event listener to slide-show arrows
+leftArrow.addEventListener("click", () => {
+  slideImage((slideIndex -= 1));
+});
+
+rightArrow.addEventListener("click", () => {
+  slideImage((slideIndex += 1));
+});
+
+// Slide images on feedback slideshow
+function slideImage(n) {
+  if (n < 0) {
+    slideIndex = feedbackBoxes.length - 1;
+  }
+
+  if (n > feedbackBoxes.length - 1) {
+    slideIndex = 0;
+  }
+
+  feedbackBoxes.forEach(v => {
+    v.classList.add("feedback__box--invisible");
+    v.classList.remove("feedback__box--visible");
+  });
+
+  feedbackBoxes[slideIndex].classList.add("feedback__box--visible");
+}
